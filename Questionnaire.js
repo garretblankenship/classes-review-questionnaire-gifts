8ccc64a994507c22a735336609255bab091293af
garretblankenship <garret.blankenship@coderacademy.edu.au>
2019-12-18 12:50:47 +1100

    checkpoint 3

--- Questionnaire.js
+++ Questionnaire.js
@@ -17,8 +17,30 @@ class Questionnaire {
     }
 
     translateRawQuestionJSON(questionJSON){
-        
+        //parse questionJSON and save in variable
+        const parsedJSON = JSON.parse(questionJSON);
+        //loop through the objects in the array
+        for (let entry of parsedJSON) {
+            const { text, options } = entry;
+            //create a new question object use the text property
+            const question = new Question(text);
+
+            //loop through options
+            for (let option of options) {
+                //add option to question
+                question.addOption(option);
+            }
+
+            //add completed question to the questionnaire questions property
+            this.addQuestion(question);
+        }       
     }
+
+    //add another method named run
+        //loop through the questions in the question list
+        //ask the user the question
+        //retrieve their response
+        //console.log their response
 }
 
 module.exports = Questionnaire;
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const questionJSON = require("./questions.json");
 const Questionnaire = require("./Questionnaire");
 
-console.log(JSON.parse(questionJSON));
-
 const kidsXmasGiftsQuestionnaire = new Questionnaire();
 kidsXmasGiftsQuestionnaire.translateRawQuestionJSON(questionJSON);
 
