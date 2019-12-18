073639e615c9d47b469d89e10c168638883cd14e
garretblankenship <garret.blankenship@coderacademy.edu.au>
2019-12-18 15:20:57 +1100

    checkpoint 5

---
+++ GiftList.js
@@ -0,0 +1,16 @@
+const Gift = require("./Gift");
+
+class GiftList {
+    constructor(queries) {
+        this.queries = queries;
+        this.list = [];
+    }
+
+    //addGift that adds the gift to the list after checking it is a Gift
+
+    //create a new method called getGiftsFromQueries
+    //this method will call the etsy api for each query
+    //put 2 random gifts into the list per query
+}
+
+module.exports = GiftList;
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const questionJSON = require("./questions.json");
 const Questionnaire = require("./Questionnaire");
+const GiftList = require("./GiftList");
 
 const kidsXmasGiftsQuestionnaire = new Questionnaire();
 kidsXmasGiftsQuestionnaire.translateRawQuestionJSON(questionJSON);
 
 const queries = kidsXmasGiftsQuestionnaire.run();
 
-//create another class in another file called GiftList that upon object creation takes in an
-//array of queries
-//save those queries in a queries property
+const kidsGiftList = new GiftList(queries);
+
+//create a class named gift that has 3 prop, title, description, price
+//that are required during object instantiation
 
 console.log(queries);
