0f0cf45ecf0341a2c1fd1c1b1a665c2b4235077c
garretblankenship <garret.blankenship@coderacademy.edu.au>
2019-12-18 12:04:37 +1100

    fixed json

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const questionJSON = require("./questions.json");
 const Questionnaire = require("./Questionnaire");
 
+console.log(questionJSON);
+
 const kidsXmasGiftsQuestionnaire = new Questionnaire();
 kidsXmasGiftsQuestionnaire.translateRawQuestionJSON(questionJSON);
 
