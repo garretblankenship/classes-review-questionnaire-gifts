89d3ca561dea4d54e4e903369fef50d598454bea
garretblankenship <garret.blankenship@coderacademy.edu.au>
2019-12-18 16:10:22 +1100

    checkpoint 6

--- GiftList.js
+++ GiftList.js
@@ -1,16 +1,50 @@
 const Gift = require("./Gift");
+const axios = require("axios");
+const { randomDistinctNumbers } = require("./utility_service");
 
 class GiftList {
     constructor(queries) {
         this.queries = queries;
         this.list = [];
+        this.getGiftsFromQueries();
     }
 
-    //addGift that adds the gift to the list after checking it is a Gift
+    addGift(gift) {
+        if (gift.constructor.name === "Gift") {
+            this.list.push(gift);
+        }
+    }
 
-    //create a new method called getGiftsFromQueries
-    //this method will call the etsy api for each query
-    //put 2 random gifts into the list per query
+    async getGiftsFromQueries() {
+        const url = "https://openapi.etsy.com/v2/listings/active?api_key=0cybldlljac0tj4lsic7bnkb&limit=5";
+
+        const promises = [];
+
+        for(let query of this.queries) {
+            promises.push(
+                axios.get(url + `&keywords=${query}`)
+            );
+        }
+
+        try {
+            let responses = await Promise.all(promises);
+                
+            for(let response of responses) {
+                const { results } = response.data;
+                const indexes = randomDistinctNumbers(results.length);
+
+            }
+        } catch(error) {
+            console.log(error);
+        }
+          
+    }
+    
+    createGiftsFromResultsIndexes(indexes, results) {
+        for(let index of indexes) {
+            let {} = results[index];
+        }
+    }
 }
 
 module.exports = GiftList;
